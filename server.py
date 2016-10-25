b16e5dcf06eee2bcfdc5fe0249f8ea49278a5c72
Weling5 <weiling588885@gmail.com>
2016-10-24 19:17:09 -0600

    index.html

--- server.py
+++ server.py
@@ -85,24 +85,32 @@ def update(entity):
         updates = flask_post_json()
         for key in updates:
             myWorld.update(entity, key, updates[key])
-    return json.dumps(myWorld.get(entity))
+    return flask.jsonify(myWorld.get(entity))
 
 
 @app.route("/world", methods=['POST','GET'])
 def world():
     '''you should probably return the world here'''
-    return json.dumps(myWorld.world())
+    if request.method == 'GET':
+        return flask.jsonify(myWorld.world())
+    elif request.method == 'POST':
+        updates = flask_post_json()
+        if updates != None:
+            for key in updates:
+                myWorld.set(entity, key, updates[key])
+        return flask.jsonify(myWorld.world())
+
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return json.dumps(myWorld.get(entity))
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
-    return json.dumps(myWorld.world())
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
